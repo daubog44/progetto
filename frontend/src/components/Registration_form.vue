ae8b8697383e894891d317a2bd03efe3afd443fe
dar3kspace <cabezasraulyunnior@gmail.com>
2026-01-20 09:37:30 +0100

    init code frontend

---
+++ frontend/src/components/Registration_form.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+</script>
+
+<style>
+    button {
+        margin-left: 120px;
+        cursor: pointer;
+        padding: 2% 5%;
+    }
+
+    button:hover {
+        background-color: lightgrey;
+
+    }
+
+    .label-size {
+        padding: 2%;
+    }
+</style>
+
+<template>
+
+    <div class="greetings">
+        <h1 class="green">Registration to Vibely</h1>
+    </div>
+    <div>
+    
+        <form action="" method="post">
+            <div class="label-size">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="label-size">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div >
+                <button type="submit">Register</button>
+            </div>
+        </form>
+    </div>
+</template>
